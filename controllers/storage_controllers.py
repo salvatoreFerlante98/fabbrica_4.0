9c9d59fd479101ea53d132d627d4dddd6122069d
Alessio Palermo <alessiopalermo@MBP-di-Alessio-001.homenet.telecomitalia.it>
2023-06-24 18:27:54 +0200

    FP-002: modified storage_controllers.py

--- controllers/storage_controllers.py
+++ controllers/storage_controllers.py
@@ -12,6 +12,9 @@ class Storage:
             self.data = data
             self.quantita = quantita
 
+        def __str__(self):
+            return "Richiesta: " + str(self.id_richiesta) + " - " + str(self.tipo) + " - " + str(self.data) + " - " + str(self.quantita) + "\n"
+
         def getId(self):
             return self.id_richiesta
 
@@ -32,10 +35,11 @@ class Storage:
     def mapIsEmpty(self):
         return len(self.richieste) == 0
 
-    def creaRichiesta(self):
+    def creaRichiesta(self, tipo, data, quantita):
         self.id += 1
-        richiesta = self.Richiesta(self.id, input("Tipo: "), input("Data: "), input("Quantità: "))
+        richiesta = self.Richiesta(self.id, tipo, data, quantita)
         self.richieste.enqueue(richiesta)
+        return richiesta
 
     def eseguiRichiesta(self):
         if self.richieste.is_empty():
@@ -48,13 +52,12 @@ class Storage:
             self.richieste.dequeue()
             print("Richiesta eseguita")
 
-    def stampaMagazzino(self):
-        for tipo in self.storage_map.keys():
-            print(tipo, " ", self.storage_map.__getitem__(tipo))
-
-    def usaPezzo(self,  tipo, quantita):
+    def usaPezzo(self, tipo, quantita):
         if self.storage_map.__getitem__(tipo) - quantita < 0:
             print("Non ci sono abbastanza pezzi")
         else:
             self.storage_map.__setitem__(tipo, self.storage_map.__getitem__(tipo) - quantita)
             print("Pezzi usati")
+
+    def getMagazzino(self):
+        return self.storage_map
