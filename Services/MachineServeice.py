6d92265f518ab654c514348e42e02ada9e83b62d
salvatoreFerlante98 <107683009+salvatoreferlante98@users.noreply.github.com>
2023-06-28 19:53:51 +0200

    FP-006: Adjusted the behaviour of the machine and created the MachineService (an interface to manage islands and machines)

---
+++ Services/MachineServeice.py
@@ -0,0 +1,14 @@
+from controllers.islands_controller import IslandsController
+
+
+# Define the states and transitions
+
+
+class MachinesService:
+
+    def __init__(self, penne):
+        self._islands = IslandsController(penne)
+
+    def
+
+
