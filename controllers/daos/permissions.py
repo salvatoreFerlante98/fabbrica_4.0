7a22d9339c831c3ed9beb9adc4efd72c441fd724
Salvatore Ferlante <salvatore.ferlante@unikorestudent.it>
2023-06-30 23:54:09 +0200

    FP-006: reformatted permissions

--- controllers/daos/permissions.py
+++ controllers/daos/permissions.py
@@ -5,8 +5,8 @@ class Permissions:
 
     def __init__(self):
         self.__permission = Tree('admin')
-        self.__permission + 'responsabile_tecnico'
-        self.__permission + 'responsabile_logistico'
+        self.__permission + 'responsabile_macchinari'
+        self.__permission + 'responsabile_logistica'
         self.__permission + 'responsabile_spedizioni'
 
     def get_permission(self, role):
