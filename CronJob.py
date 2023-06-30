442be15033fc48dabae30f0378bde3da6383d025
Salvatore Ferlante <salvatore.ferlante@unikorestudent.it>
2023-06-30 16:25:54 +0200

    FP-006: removed useless comments

--- CronJob.py
+++ CronJob.py
@@ -11,9 +11,9 @@ class Cronjob:
         self._richiesta_penne = 1000
 
     @staticmethod
-    def check_status_and_create_request(magazzino, consumo, risorsa, penne_da_produrre):
+    def check_status_and_choose_machines(prodotto, consumo, risorsa, penne_da_produrre):
         n_macchinari = round(
-            (magazzino / penne_da_produrre) * 40
+            (prodotto / penne_da_produrre) * 40
         )
         while (risorsa < (consumo * n_macchinari)) or (n_macchinari == 0):
             n_macchinari -= 1
@@ -30,18 +30,18 @@ class Cronjob:
 
         penne_da_produrre = self._richiesta_penne - self._storages.get_magazzino('penne')
 
-        n_macchinari = self.check_status_and_create_request(self._storages.get_magazzino('punte'),
-                                                            self._islands.get_consume('punte'), self._storages.get_magazzino('metallo'), penne_da_produrre)
+        n_macchinari = self.check_status_and_choose_machines(self._storages.get_magazzino('punte'),
+                                                             self._islands.get_consume('punte'), self._storages.get_magazzino('metallo'), penne_da_produrre)
         if n_macchinari > 0:
             self._islands.crea_richiesta('punte', n_macchinari)
 
-        n_macchinari = self.check_status_and_create_request(self._storages.get_magazzino('astucci'),
-                                                            self._islands.get_consume('astucci'), self._storages.get_magazzino('plastica'), penne_da_produrre)
+        n_macchinari = self.check_status_and_choose_machines(self._storages.get_magazzino('astucci'),
+                                                             self._islands.get_consume('astucci'), self._storages.get_magazzino('plastica'), penne_da_produrre)
         if n_macchinari > 0:
             self._islands.crea_richiesta('astucci', n_macchinari)
 
-        n_macchinari = self.check_status_and_create_request(self._storages.get_magazzino('tappi'),
-                                                            self._islands.get_consume('tappi'), self._storages.get_magazzino('plastica'), penne_da_produrre)
+        n_macchinari = self.check_status_and_choose_machines(self._storages.get_magazzino('tappi'),
+                                                             self._islands.get_consume('tappi'), self._storages.get_magazzino('plastica'), penne_da_produrre)
         if n_macchinari > 0:
             self._islands.crea_richiesta('tappi', n_macchinari)
 
@@ -49,21 +49,18 @@ class Cronjob:
             (self._storages.get_magazzino('penne') / (
                     self._richiesta_penne - self._storages.get_magazzino('penne'))) * 40
         )
-        while (self._storages.get_magazzino('punte') < (
-                1 * n_macchinari)) and (
-                self._storages.get_magazzino('tappi') < (
-                1 * n_macchinari)) and (
-                self._storages.get_magazzino('astucci') < (
-                1 * n_macchinari)) or (n_macchinari > 0):
+        while (self._storages.get_magazzino('punte') < n_macchinari) and (
+                self._storages.get_magazzino('tappi') < n_macchinari) and (
+                self._storages.get_magazzino('astucci') < n_macchinari) \
+                or (n_macchinari > 0):
             n_macchinari -= 1
         if n_macchinari > 0:
             self._islands.crea_richiesta('penne', n_macchinari)
 
 
 cronjob = Cronjob()
-schedule.every(1).seconds.do(cronjob.my_task)  # Use the method from the cronjob instance
+schedule.every(1).seconds.do(cronjob.my_task)
 
-# Run the scheduler
 while True:
     schedule.run_pending()
     time.sleep(1)
