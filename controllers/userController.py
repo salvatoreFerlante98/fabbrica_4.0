8a4b305a7c60906b6eb7e378504aa34fd9605a79
Salvatore Ferlante <salvatore.ferlante@unikorestudent.it>
2023-07-01 14:12:24 +0200

    FP-006: Refactored userController to be more lintiness

--- controllers/userController.py
+++ controllers/userController.py
@@ -6,23 +6,27 @@ from controllers.daos.permissions import Permissions
 class UserController:
 
     def __init__(self):
-        self.__users = ProbeHashMap()
+        self._users = ProbeHashMap()
         self.__permission = Permissions()
 
     def add_user(self, name, password, role):
-        self.__users[name] = User(name, self.__permission.get_permission(role), password)
+        self._users[name] = User(name, self.__permission.get_permission(role), password)
 
     def del_user(self, name):
-        del self.__users[name]
+        del self._users[name]
 
     def __str__(self):
-        return str(self.__users)
+        return str(self._users)
 
     def login(self, name, password):
-        if self.__users[name].check_password(password):
-            return self.__users[name]
+        if self._users[name].check_password(password):
+            return self._users[name]
         else:
             return None
 
     def __getitem__(self, item):
-        return self.__users[item]
+        return self._users[item]
+
+    def __iter__(self):
+        for user in self._users:
+            yield user
